57c568867cad31fd39158453ecd277155cb848a0
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2021-12-17 14:30:32 +0000

    added whiteboard state matrix to store the state of the whiteboard externally; added color picker, clear button and eraser button

--- public/whiteboard.js
+++ public/whiteboard.js
@@ -1,46 +1,80 @@
+const COLOR_PICKER = document.getElementById("color-picker");
 const WHITEBOARD = document.getElementById("whiteboard");
+const CLEAR_BUTN = document.getElementById("clear-butn");
 const TABLE_ROWS = 80;
 const TABLE_COLS = 80;
+const ERASER = document.getElementById("eraser");
 let mouseIsDown = false;
+let currentColor = "#0000ff";
+
+COLOR_PICKER.addEventListener("change", (e) => currentColor = e.target.value);
+CLEAR_BUTN.addEventListener("click", () => {
+    WHITEBOARD.innerHTML = "";
+    generateWhiteboard();
+    generateDefaultWhiteboardState();
+})
+ERASER.addEventListener("click", () => {currentColor="#f1f1f1", COLOR_PICKER.value = "#f1f1f1"})
 
 document.body.onmousedown = e => {
     if(e.button == 0){ mouseIsDown = true; }
     else { mouseIsDown = false; }
 }
 document.body.onmouseup = () => {
-    mouseIsDown = false;    
+    mouseIsDown = false;
 }
 
 // @todo generate whiteboard from json object
 
+let whiteboardState = [];
+function generateDefaultWhiteboardState() {
+    for (let i = 0; i < TABLE_ROWS; i++) {
+        let row = [];
+        for (let col = 0; col < TABLE_COLS; col++) {
+            row.push({
+                row: i,
+                col,
+                color: "#f1f1f1",
+            });
+        }
+        whiteboardState.push(row);
+    }
+}
+
+generateDefaultWhiteboardState();
+
 // generates html table element
-function generateWhiteboardTable() {
-    for(i=0; i<TABLE_ROWS; i++) {
+function generateWhiteboard() {
+    for(i = 0; i < TABLE_ROWS; i++) {
         let row = document.createElement("tr");
         row.id = `${i}`;
         WHITEBOARD.appendChild(row);
 
-        for(j=0; j<TABLE_COLS; j++) {
+        for(j = 0; j < TABLE_COLS; j++) {
             let td = document.createElement("td");
-            td.id = `col-${j} row-${i}`;
+            td.id = `${i} ${j}`;
             td.addEventListener("mouseover", (e) => {
-                if(mouseIsDown) {
-                    td.style.background = "blue"
-                }
+                if(mouseIsDown) {onChange(e)};
             })
-            td.addEventListener("click", () => td.style.background = "blue")
+            td.addEventListener("click", (e) => onChange(e));
 
             row.appendChild(td);
         }
     }
 }
 
-generateWhiteboardTable();
+generateWhiteboard();
 
-/**
- * [{
- *  row: 0
- *  column: 2
- * color: blue
- * }]
- */
+function onChange(e) {
+    let [row, col] = e.target.id.split(" ");
+    whiteboardState[row][col] = {
+        row,
+        col,
+        colour: currentColor,
+    };
+
+    updateCell(row, col)
+}
+
+function updateCell(row, col) {
+    WHITEBOARD.childNodes[row].childNodes[col].style.background = currentColor;
+}
